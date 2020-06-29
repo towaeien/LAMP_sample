4ccb6e246c45f730e5117a60e83e9c76836a47bb
towaeien <t.kikuchi.sys19@morijyobi.ac.jp>
2020-06-29 12:03:48 +0900

    add

---
+++ delete.php
@@ -0,0 +1,22 @@
+<?php
+$dsn = 'mysql:dbname=sample_db;host=localhost;';
+$user = 'towa';
+$password = 'morijyobi';
+try {
+    $dbh = new PDO($dsn, $user, $password);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $id = $_GET['id'];
+
+    $sql = "delete form user where id=:id";
+    $stmt = $dbh->prepare($sql);
+    $prams = array(':id'=> $id);
+    $stmt->execute($prams);
+
+    header('Location: index.php?fg=1');
+    
+} catch (PDOException $e) {
+	header('Location: index.php?fg=2?err='.$e->getMessage());
+	exit();
+}
+?>
